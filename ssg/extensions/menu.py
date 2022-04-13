b3cf091ce7d8dc881de93fcf9aa73776ea57ec54
= <=>
2022-04-13 14:00:34 -0400

    Completed module

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -14,7 +14,7 @@ def collect_files (source, site_parser):
 def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
     menu_item = lambda name, ext: template.format(name, ext, "Name")
-    menu =  = [menu_item(path.stem, ext) for path in files]
+    menu = [menu_item(path.stem, ext) for path in files]
     "\n".join(menu)
     return "<ul>\n{}<ul>\n{}".format(menu, html)
 
